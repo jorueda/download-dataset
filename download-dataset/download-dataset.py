fff04414e7d09cca5a1e78ac1ac5e21214d45c5c
Jorge <jorgeru045@gmail.com>
2019-07-30 14:40:34 -0500

    Checking recursive script with  _DOCS folder

--- download-dataset/download-dataset.py
+++ download-dataset/download-dataset.py
@@ -1,20 +1,21 @@
 # Download dataset using wget
 
+# Parameters to be used with wget
+username='nedc_tuh_eeg'
+password='nedc_tuh_eeg'
+webpage='https://www.isip.piconepress.com/projects/tuh_eeg/downloads/tuh_eeg_seizure/v1.5.0/_DOCS/'
+
 # Import libraries
 from datetime import datetime
 import os
 
-# Parameters to be used with wget
-username='nedc_tuh_eeg'
-password='nedc_tuh_eeg'
-webpage='https://www.isip.piconepress.com/projects/tuh_eeg/downloads/tuh_eeg_seizure/v1.5.0/'
-
 # Create target directory with current date and time
 folder_name=datetime.now().isoformat()
 os.mkdir(folder_name)
+os.chdir(folder_name)
 
 # wget
-wget_command='wget --user='+username+' --password='+password+' '+webpage
+wget_command='wget --recursive --no-parent --reject "index.html*" --user='+username+' --password='+password+' '+webpage
 print(wget_command)
 os.system(wget_command)
 print('Folder \"'+folder_name+'\" created in path \"'+os.getcwd()+'\"')

772d7989eca6b05e46952158a063665e8284be87
Jorge <jorgeru045@gmail.com>
2019-07-31 08:56:22 -0500

    Final script including depth level recursion

--- download-dataset/download-dataset.py
+++ download-dataset/download-dataset.py
@@ -3,7 +3,7 @@
 # Parameters to be used with wget
 username='nedc_tuh_eeg'
 password='nedc_tuh_eeg'
-webpage='https://www.isip.piconepress.com/projects/tuh_eeg/downloads/tuh_eeg_seizure/v1.5.0/_DOCS/'
+webpage='https://www.isip.piconepress.com/projects/tuh_eeg/downloads/tuh_eeg_seizure/v1.5.0/'
 
 # Import libraries
 from datetime import datetime
@@ -15,7 +15,7 @@ os.mkdir(folder_name)
 os.chdir(folder_name)
 
 # wget
-wget_command='wget --recursive --no-parent --reject "index.html*" --user='+username+' --password='+password+' '+webpage
-print(wget_command)
+wget_command='wget --recursive --level=0 --no-parent --reject "index.html*" --user='+username+' --password='+password+' '+webpage
 os.system(wget_command)
+print('\n')
 print('Folder \"'+folder_name+'\" created in path \"'+os.getcwd()+'\"')
